545d1062abede61948de3b47a4d73ba78b05b4d8
ghizlane <g.elkarroumi>
2024-04-25 16:32:48 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: true,
+    sourcemap: false,
+  },
+  base: "/react-vite-travel-advisor/",
   plugins: [react()],
 })
